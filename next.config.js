64572ed09205082d3e5d29df47a970edda167e7c
Abdelhameed Atta <meedoouu199735@gmail.com>
2026-06-10 20:46:16 +0400

    Fix pdfmake SSR: exclude browser-only bundles from server build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.resolve.alias['pdfmake/build/pdfmake'] = false;
+      config.resolve.alias['pdfmake/build/vfs_fonts'] = false;
+    }
+    return config;
+  },
+};
 
 module.exports = nextConfig
